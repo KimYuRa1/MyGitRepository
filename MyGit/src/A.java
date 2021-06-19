0da1ee1975e04f4055e89320246f59f87fd612b5
user <ura3118@naver.com>
2021-06-19 19:38:28 +0900

    devB change

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -2,5 +2,7 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		B b=new B();
+		b.sayHello();
 	}
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -2,5 +2,6 @@
 public class B {
 	public void sayHello() {
 		System.out.println("Hello !!");
+		System.out.println("Hello2 !!");
 	}
 }
