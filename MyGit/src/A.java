b1c4e1f95f92e30575d47ba2cce16d23ba22463d
user <ura3118@naver.com>
2021-06-19 19:45:15 +0900

    Change in devC branch

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -2,6 +2,8 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("Change in Master branch!");
+		C c=new C();
+		c.sayHaha();
+		System.out.println("Change in devC branch!");
 	}
 }
